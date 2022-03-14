8d5cf465a8dc47d4ec6bea9878029220cd5e8a18
xJeris <85350673+xJeris@users.noreply.github.com>
2022-03-14 09:24:01 -0400

    updated challenge files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,9 @@
 library(tidyverse)
 library(dplyr)
 
-# Load CSV file
+
+# Deliverable 1
+# Load CSV file (MechaCar MPG)
 mechaCar <- read_csv("BC-MechaCar_Stat_Analysis/MechaCar_mpg.csv")
 
 # Perform line regression
@@ -10,4 +12,15 @@ reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearan
 
 # Create summary
 summary(reg)
+
+
+# Deliverable 2
+# Load CSV file (Suspension Coil)
+suspension <- read_csv("BC-MechaCar_Stat_Analysis/Suspension_Coil.csv")
+
+# Create total summary data frame
+susp_tot_summary <- suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Create lot summary data frame
+sus_lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
